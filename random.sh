d803ed9b3173ba3b4af860496d3410569f4fdc28
Angell6991 <miguel9196.guzman@gmail.com>
2025-03-08 23:01:24 -0500

    config_001

--- random.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-wallpaper_dir="/home/angell/Imágenes/.wallpaper/Escritorio"  # Reemplaza con la ruta a tu carpeta de fondos
-wallpapers=($wallpaper_dir/*)
-wallpaper=${wallpapers[RANDOM % ${#wallpapers[@]}]}
-nitrogen --set-zoom-fill --random $wallpaper 
